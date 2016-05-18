b368838267a098face8843235e836d9e693d73d4
Denis <dkorytkin@gmail.com>
2016-05-18 21:32:08 +0300

    create func is_message and is_text

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #  -*- coding: utf8 -*-
 __author__ = 'Denis'
 
+
 import time
 import json
 from token import SLACK_TOKEN
@@ -10,21 +11,39 @@ from slackclient import SlackClient
 
 with open('questions.json', 'r') as file_questions:
     questions = json.load(file_questions)
-print(questions)
+questions_keys = list(questions.keys())
+
+
+def is_message(text):
+    if text[0]['type'] == 'message':
+        return text[0]
+
+
+def is_text(text):
+    return text[0]['text'].lower()
+
 
 sc = SlackClient(SLACK_TOKEN)
 if sc.rtm_connect():
-    print(sc.api_call('api.test'))
-    print(sc.api_call('im.open', user='U0DF0B546'))
+
     while True:
-        text = sc.rtm_read()
-        if text:
-            if text[0]['type'] == 'message':
-                print(text)
+        if sc.rtm_read():
+            req = sc.rtm_read()
+            if is_message(req):
+                print(req)
                 time.sleep(1)
-                if text[0]['text'].lower() == u'привет!':
-                    sc.api_call('chat.postMessage', channel=text[0]['channel'], text=u'Привет, как дела?', username='Mario', icon_emoji=':mario:')
+                if is_text(req) in questions_keys:
+                    key = is_text(req)
+                    sc.api_call(
+                        'chat.postMessage',
+                        channel=req[0]['channel'],
+                        text=questions[key],
+                        username='Mario',
+                        icon_emoji=':mario:'
+                    )
 else:
     print("Connection Failed, invalid token?")
 
 
+# print(sc.api_call('api.test'))
+# print(sc.api_call('im.open', user='U0DF0B546'))
